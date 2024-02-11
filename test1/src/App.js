d653c4f5504b372947bbc35d7312200f11726124
Himanshoo Thakre <99898464+thakrehimanshoo@users.noreply.github.com>
2024-02-11 15:05:23 +0530

    app

---
+++ test1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar  from './components/navbar';
+
+function App() {
+  return (
+   <div>
+     <NavBar />
+    <h1>Hello, React!</h1>
+   </div>
+  );
+}
+
+export default App;
